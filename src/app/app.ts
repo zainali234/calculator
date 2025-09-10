9459358bd05e7a38065c271b635b13d8c9b2df8e
Malik Zain <zainaliawan187@gmail.com>
2025-09-10 10:02:27 +0500

    done

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,32 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { signal } from '@angular/core';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [ CommonModule],
   templateUrl: './app.html',
+  
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('calculator');
+
+  stringToEvaluate:string = '';
+
+  takeInput(num:string){
+    this.stringToEvaluate = this.stringToEvaluate + num;
+    console.log(this.stringToEvaluate);
+  }
+
+  evaluateResult(){
+   if(this.stringToEvaluate != ''){
+    this.stringToEvaluate = eval(this.stringToEvaluate)
+   }
+  }
+
+  resetInput(){
+    this.stringToEvaluate = '';
+  }
 }
